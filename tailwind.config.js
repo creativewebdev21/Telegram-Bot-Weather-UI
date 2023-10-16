6bf839d337eb2d039918143db8b76d1c6b9852a0
Ronald <ronaldpelham51@outlook.com>
2023-10-16 08:16:39 -0400

    I init project

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/* eslint-disable global-require */
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
+    "./shared/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+       
+      },
+      screens: {
+        ios : '320px',
+        samsungS8: "360px",
+        xs: '390px',
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl":'1448px',
+        "3xl": "1920px",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      display: ["dark"],
+    },
+  },
+  darkMode: ["class"],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("tailwind-scrollbar")({ nocompatible: true }),
+    // ...
+  ],
+}
