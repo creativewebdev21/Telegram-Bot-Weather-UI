252f7ee38b33cf18071068813dbde6eadaacda9c
Software Engineer <henry.ziad.engineer@gmail.com>
2023-10-16 15:17:46 -0400

    I add getAdminByUsername endpoint

--- src/server/controller/admin.post.controller.ts
+++ src/server/controller/admin.post.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from "@nestjs/common"
+import { Controller, Post, Body } from "@nestjs/common"
+import Admin from "../model/admin.model"
 
 @Controller()
 export class AdminPostController {
   // eslint-disable-next-line no-useless-constructor, no-empty-function
   constructor() {}
+
+  @Post("/api/admin/getOne")
+  public async getAdminByUserName(@Body() body: { username: string }) {
+    const { username } = body
+
+    try {
+      const admin = await Admin.findOne({ username }).lean()
+
+      return admin as any
+    } catch (err) {
+      throw new Error(err)
+    }
+  }
 }
