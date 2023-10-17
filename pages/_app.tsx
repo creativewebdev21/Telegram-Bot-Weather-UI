a08c091752c1a73ef6980b759cc75d888e858451
Ronald <ronaldpelham51@outlook.com>
2023-10-17 15:05:35 -0400

    I complete google login & writing admin data into db

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import "../styles/globals.css"
 import React from "react"
 import type { AppProps } from "next/app"
+import { GoogleOAuthProvider } from "@react-oauth/google"
+import { UserProvider } from "../providers/UserProvider"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <GoogleOAuthProvider
+        clientId={
+          process.env.NEXT_PUBLIE_GOOGLE_CLIENT_ID ||
+          "551764290924-trda8mpkcpr4445cf7t06g06saeu5lcp.apps.googleusercontent.com"
+        }
+      >
+        <Component {...pageProps} />
+      </GoogleOAuthProvider>
+    </UserProvider>
+  )
 }
 export default MyApp
