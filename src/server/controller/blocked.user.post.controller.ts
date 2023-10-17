6f7bb37de24000986b3600e6ebf161b451c15b27
Software Engineer <henry.ziad.engineer@gmail.com>
2023-10-17 08:01:27 -0400

    I add blocked user endpoint

--- src/server/controller/blocked.user.post.controller.ts
+++ src/server/controller/blocked.user.post.controller.ts
@@ -1,15 +1,30 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Post, Body } from "@nestjs/common"
+import BlockedUser from "../model/blocked.user.model"
+import Subscriber from "../model/subscriber.model"
 
 @Controller()
 export class BlockedUserPostController {
-    constructor() {}
+  // eslint-disable-next-line no-useless-constructor, no-empty-function
+  constructor() {}
 
-    @Get('/api/admin/blocked')
-    public async blockedUser {
-        try {
+  @Post("/api/admin/blocked")
+  public async blockedUser(@Body() body: { userid: string; username: string }) {
+    const { userid, username } = body
 
-        } catch(err) {
-            throw new Error(err)
-        }
+    try {
+      await Subscriber.findOneAndDelete({ userid })
+
+      const Blocked = new BlockedUser({
+        userid,
+        username,
+      })
+
+      const blockedUser = await Blocked.save()
+
+      return blockedUser as any
+      return "daaa"
+    } catch (err) {
+      throw new Error(err)
     }
+  }
 }
