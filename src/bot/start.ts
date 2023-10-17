b237db91e3d1c721e669c0c79861d8291cee566a
Software Engineer <henry.ziad.engineer@gmail.com>
2023-10-17 16:49:25 -0400

    I add help command of bot

---
+++ src/bot/start.ts
@@ -0,0 +1,20 @@
+import User from '../model/UserModel'
+
+const start = (bot: any) => {
+    bot.start(async (ctx: any) => {
+        const userId = ctx.from.id;
+        const username = ctx.from.username;
+        const user = new User({
+            username: username,
+            userid: userId,
+        });
+        try {
+            await user.save()
+            ctx.reply('Welcome to the WEATHER UPDATE bot!\nThis is a publicly available bot made by Henry to get the weather information of a particular city of your choice... \n\n\nTap /help - To Get help with commands\n\nMade with ❤️ by Henry Ziad');
+        } catch(err: any) {
+            throw new Error(err)
+        }
+    });
+}
+
+export default start
