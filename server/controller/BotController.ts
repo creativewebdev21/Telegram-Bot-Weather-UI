cd9f5c0ea0cc37f74610ea8ba4a94d6bbc62af35
Ronald <ronaldpelham51@outlook.com>
2023-10-18 11:00:41 -0400

    I add relaunch bot endpoint

--- server/controller/BotController.ts
+++ server/controller/BotController.ts
@@ -1,5 +1,6 @@
-import { Controller, Post, Body } from "@nestjs/common"
+import { Controller, Post, Body, Get } from "@nestjs/common"
 import BotModel from "../model/BotModel"
+import launch, { stop } from "../bot/WeatherBot"
 
 @Controller()
 class BotController {
@@ -14,6 +15,34 @@ class BotController {
       const bot = await BotModel.findOneAndUpdate({ key }).lean()
 
       return bot as any
+    } catch (err: any) {
+      throw new Error(err)
+    }
+  }
+
+  @Post("/api/bot/relanuch")
+  public async relaunch(@Body() body: { newkey: string; oldKey: string }) {
+    const { newkey, oldKey } = body
+
+    try {
+      stop(oldKey)
+
+      await BotModel.findOneAndUpdate({ key: oldKey }, { key: newkey }).lean()
+
+      launch(newkey)
+    } catch (err: any) {
+      throw new Error(err)
+    }
+  }
+
+  @Get("/api/bot/getKey")
+  public async getKey() {
+    try {
+      const bots = await BotModel.find({})
+
+      if (bots.length) return bots[0]
+
+      return null
     } catch (err: any) {
       throw new Error(err)
     }
